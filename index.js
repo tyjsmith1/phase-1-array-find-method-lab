440b1c62c504d56ac3bb8fa67401a43ca34191d0
tylersmith <tyjsmith1@gmail.com>
2023-11-01 13:45:03 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(games) {
+    const winObject = games.find(game => game.result === "W")
+
+    if (winObject) {
+        return winObject.year
+    } else {
+        return undefined
+    }
+}
